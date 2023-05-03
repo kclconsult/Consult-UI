5003beaa87b13c89a5321411278dc3f65b98603b
Cyano0 <36192489+Cyano0@users.noreply.github.com>
2023-05-03 14:05:56 +0100

    Add files via upload

--- js/consult.js
+++ js/consult.js
@@ -21,30 +21,41 @@ var callAPISubmitFeedback = (feedbackInput,userName)=>{
         redirect: 'follow'
     };
     // make API call with parameters and use promises to get response
-    fetch("https://3r5biyqpg0.execute-api.eu-north-1.amazonaws.com/dev", requestOptions) //TODO: to changed the API
+    fetch("https://trtbhfsrkl.execute-api.eu-west-2.amazonaws.com/dev", requestOptions) //TODO: to changed the API
     .then(response => response.text())
     .then(result => alert(JSON.parse(result).body))
     .catch(error => console.log('error', error));
 }
 
-// define the callAPI function that takes a first name and last name as parameters
-var callAPI = (firstName,lastName)=>{
-    // instantiate a headers object
-    var myHeaders = new Headers();
-    // add content type header to object
-    myHeaders.append("Content-Type", "application/json");
-    // using built in JSON utility package turn object to string and store in a variable
-    var raw = JSON.stringify({"firstName":firstName,"lastName":lastName});
-    // create a JSON object with parameters for API call and store in a variable
-    var requestOptions = {
-        method: 'POST',
-        headers: myHeaders,
-        body: raw,
-        redirect: 'follow'
-    };
-    // make API call with parameters and use promises to get response
-    fetch("https://3r5biyqpg0.execute-api.eu-north-1.amazonaws.com/dev", requestOptions)
-    .then(response => response.text())
-    .then(result => alert(JSON.parse(result).body))
-    .catch(error => console.log('error', error));
+
+// https://html.form.guide/action/html-form-action-javascript-example/
+// Submit data EXAMPLE, for test. FEEL FREE TO DELETE
+function submitFormPHQ2(e) {
+    e.preventDefault();   
+    var myform = document.getElementById("wf-form-phq2-form-block");   
+    var formData = new FormData(myform);
+  
+    fetch("https://show.ratufa.io/json", {
+      method: "POST",
+      body: formData,
+    })
+      .then(response => {
+      if (!response.ok) {
+        throw new Error('network returns error');
+      }
+      return response.json();
+    })
+      .then((resp) => {
+        let respdiv = document.createElement("pre");
+        respdiv.innerHTML = JSON.stringify(resp, null, 2);
+        myform.replaceWith(respdiv);
+        console.log("resp from server ", resp);
+      })
+      .catch((error) => {
+        // Handle error
+        console.log("error ", error);
+      });
 }
+  
+var phq2FormSubmit = document.getElementById("submit-phq2-form");
+phq2FormSubmit.addEventListener("submit", submitFormPHQ2);
